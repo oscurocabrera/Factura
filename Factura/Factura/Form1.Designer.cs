78d914cb71bda7755f221708d8829a65e42b4e6f
oscurocabrera <oscurocabrera@gmail.com>
2023-06-25 23:43:27 -0600

    proyecto formato

---
+++ Factura/Factura/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿
+namespace Factura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblTotalpagar = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblDevolucion = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnVender = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.txtEfectivo = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.txtEfectivo);
+            this.panel1.Controls.Add(this.cmbProducto);
+            this.panel1.Controls.Add(this.dgvLista);
+            this.panel1.Controls.Add(this.txtCantidad);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.lblDevolucion);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.lblTotalpagar);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblNombre);
+            this.panel1.Controls.Add(this.lblCodigo);
+            this.panel1.Controls.Add(this.lblPrecio);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(46, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(690, 630);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRODUCTO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CODIGO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NOMBRE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PRECIO:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(154, 181);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(14, 17);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "-";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(154, 101);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(14, 17);
+            this.lblCodigo.TabIndex = 5;
+            this.lblCodigo.Text = "-";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(154, 139);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(14, 17);
+            this.lblNombre.TabIndex = 6;
+            this.lblNombre.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(50, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "CANTIDAD:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(50, 433);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "TOTAL A PAGAR:";
+            // 
+            // lblTotalpagar
+            // 
+            this.lblTotalpagar.AutoSize = true;
+            this.lblTotalpagar.Location = new System.Drawing.Point(50, 483);
+            this.lblTotalpagar.Name = "lblTotalpagar";
+            this.lblTotalpagar.Size = new System.Drawing.Size(14, 17);
+            this.lblTotalpagar.TabIndex = 9;
+            this.lblTotalpagar.Text = "-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(228, 433);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(88, 17);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "EFECTIVO:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(383, 433);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(113, 17);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "DEVOLUCIÓN:";
+            // 
+            // lblDevolucion
+            // 
+            this.lblDevolucion.AutoSize = true;
+            this.lblDevolucion.Location = new System.Drawing.Point(346, 483);
+            this.lblDevolucion.Name = "lblDevolucion";
+            this.lblDevolucion.Size = new System.Drawing.Size(14, 17);
+            this.lblDevolucion.TabIndex = 12;
+            this.lblDevolucion.Text = "-";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(12, 9);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(759, 39);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "FACTURA REFACCIONARIA LOS CABRERA";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregar.Location = new System.Drawing.Point(422, 93);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(204, 54);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEliminar.Location = new System.Drawing.Point(422, 164);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(204, 53);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnVender
+            // 
+            this.btnVender.BackColor = System.Drawing.Color.Lime;
+            this.btnVender.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVender.Location = new System.Drawing.Point(229, 713);
+            this.btnVender.Name = "btnVender";
+            this.btnVender.Size = new System.Drawing.Size(273, 61);
+            this.btnVender.TabIndex = 14;
+            this.btnVender.Text = "VENDER";
+            this.btnVender.UseVisualStyleBackColor = false;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(170, 205);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(134, 22);
+            this.txtCantidad.TabIndex = 15;
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLista.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvLista.Location = new System.Drawing.Point(30, 255);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
+            this.dgvLista.RowHeadersVisible = false;
+            this.dgvLista.RowHeadersWidth = 51;
+            this.dgvLista.RowTemplate.Height = 24;
+            this.dgvLista.Size = new System.Drawing.Size(620, 163);
+            this.dgvLista.TabIndex = 16;
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Items.AddRange(new object[] {
+            "Motor",
+            "Chasis",
+            "Transmisiones"});
+            this.cmbProducto.Location = new System.Drawing.Point(162, 54);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(223, 24);
+            this.cmbProducto.TabIndex = 17;
+            this.cmbProducto.SelectedIndexChanged += new System.EventHandler(this.cmbProducto_SelectedIndexChanged);
+            // 
+            // txtEfectivo
+            // 
+            this.txtEfectivo.Location = new System.Drawing.Point(157, 483);
+            this.txtEfectivo.Name = "txtEfectivo";
+            this.txtEfectivo.Size = new System.Drawing.Size(147, 22);
+            this.txtEfectivo.TabIndex = 18;
+            this.txtEfectivo.TextChanged += new System.EventHandler(this.txtEfectivo_TextChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "CODIGO";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "PRODUCTO";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "PRECIO";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "CANTIDAD";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "TOTAL";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 786);
+            this.Controls.Add(this.btnVender);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "REFACCIONARIA LOS CABRERA";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtEfectivo;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblDevolucion;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblTotalpagar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btnVender;
+    }
+}
+
---
+++ Factura/Factura/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int cod;
+            string nom;
+            float precio;
+
+            cod = cmbProducto.SelectedIndex;
+            nom = cmbProducto.SelectedItem.ToString();
+            precio = cmbProducto.SelectedIndex;
+
+            switch (cod)
+            {
+                case 0: lblCodigo.Text = "0011";break;
+                case 1: lblCodigo.Text = "0022";break;
+                default: lblCodigo.Text = "0033";break;
+            }
+
+            switch (nom)
+            {
+                case "Motor": lblNombre.Text = "Motor";break;
+                case "Chasis": lblNombre.Text = "Chasis";break;
+                default: lblNombre.Text = "Transmisiones"; break;
+            }
+
+            switch (precio)
+            {
+                case 0: lblPrecio.Text = "15000";break;
+                case 1: lblPrecio.Text = "5500";break;
+                default: lblPrecio.Text = "13000";break;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow file = new DataGridViewRow();
+            file.CreateCells(dgvLista);
+
+            file.Cells[0].Value = lblCodigo.Text;
+            file.Cells[1].Value = lblNombre.Text;
+            file.Cells[2].Value = lblPrecio.Text;
+            file.Cells[3].Value = txtCantidad.Text;
+            file.Cells[4].Value = (float.Parse(lblPrecio.Text) * float.Parse(txtCantidad.Text)).ToString();
+
+            dgvLista.Rows.Add(file);
+
+            lblCodigo.Text = lblNombre.Text = lblPrecio.Text = txtCantidad.Text = "";
+
+            obtenerTotal();
+        }
+
+        public void obtenerTotal()
+        {
+            float costot = 0;
+            int contador = 0;
+
+            contador = dgvLista.RowCount;
+
+            for(int i=0; i<contador; i++)
+           {
+                costot += float.Parse(dgvLista.Rows[i].Cells[4].Value.ToString());
+            }
+
+            lblTotalpagar.Text = costot.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try {
+                DialogResult rppta = MessageBox.Show("¿Desea eliminar producto?",
+                    "Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if(rppta == DialogResult.Yes)
+                {
+                    dgvLista.Rows.Remove(dgvLista.CurrentRow);
+                }
+            }
+            catch { }
+            obtenerTotal();
+        }
+
+        private void txtEfectivo_TextChanged(object sender, EventArgs e)
+        {
+            try {
+                lblDevolucion.Text = (float.Parse(txtEfectivo.Text) - float.Parse(lblTotalpagar.Text)).ToString();
+            }
+            catch { }
+        }
+    }
+}
